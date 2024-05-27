2a660d776b53f3810da3f50db17f05dab6ec9944
Jack Richard White <jrw3978@ads.northwestern.edu>
2024-05-27 14:07:39 -0500

    attempt 2.

--- attempt_2/2.5_assess_final_model.R
+++ attempt_2/2.5_assess_final_model.R
@@ -5,18 +5,18 @@ library(tidymodels)
 library(here)
 
 # load data
-load(here("attempt_1/results/final_fit.rda"))
+load(here("attempt_2/results/final_fit_2.rda"))
 load(here("data/test_classification.rda"))
 
-predicted_prob_en_1 <-  predict(final_fit, test_classification, type = "prob")
+predicted_prob_bt_1 <-  predict(final_fit_2, test_classification, type = "prob")
 
-prob_result_en_1 <- test_classification |>
+prob_result_bt_1 <- test_classification |>
   select(id) |>
-  bind_cols(predicted_prob_en_1) |>
+  bind_cols(predicted_prob_bt_1) |>
   select(id, .pred_TRUE) |>
   rename(predicted = .pred_TRUE)
 
-prob_result_en_1
+prob_result_bt_1
 
 # making boosted tree submission
-write_csv(prob_result_en_1, file = here("attempt_1/submission/en_submission_attempt_1.csv"))
+write_csv(prob_result_bt_1, file = here("attempt_2/submission/bt_submission_attempt_2.csv"))
