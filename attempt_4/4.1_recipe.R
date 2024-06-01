65f365f7d64390b083126309dcf1ba889902ee94
jackwhite2026 <145519585+jackwhite2026@users.noreply.github.com>
2024-05-31 22:57:13 -0500

    attempt 4

--- attempt_4/4.1_recipe.R
+++ attempt_4/4.1_recipe.R
@@ -15,11 +15,7 @@ load(here("data/train_classification.rda"))
 
 # creating kitchen sink recipe ----
 classification_recipe_2 <- recipe(host_is_superhost ~., data = train_classification) |> 
-  step_rm(first_review, last_review, review_scores_cleanliness, review_scores_communication, reviews_per_month, 
-          review_scores_rating, review_scores_accuracy, review_scores_checkin, review_scores_location, 
-          review_scores_value, host_location, host_neighbourhood, id, host_since, first_review, last_review) |> 
-  # getting rid of all the variables that have more than 20% missingness
-  # getting rid of id and date variables
+  step_rm(id) |> 
   step_impute_mode(all_nominal_predictors()) |>
   step_impute_median(all_numeric_predictors()) |>
   step_novel(all_nominal_predictors()) |> 
--- attempt_4/4.3_model_analysis.R
+++ attempt_4/4.3_model_analysis.R
@@ -15,10 +15,10 @@ load(here("attempt_4/results/btree_tuned_2.rda"))
 
 # creating individual tables 
 
-tbl_bt_1 <- show_best(btree_tuned_2, metric = "roc_auc") |> 
+tbl_bt_2 <- show_best(btree_tuned_2, metric = "roc_auc") |> 
   slice_min(mean) |> 
   select(mean, n, std_err) |> 
   mutate(model = "Boosted Tree 2")
 
 # save results table
-save(tbl_bt_1, file = here("attempt_4/results/results_table.rda"))
+save(tbl_bt_2, file = here("attempt_4/results/results_table.rda"))
